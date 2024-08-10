305b886ca759c12693f6ae2110e90f136963cea3
baran kalaycioglu <usbarank@gmail.com>
2024-08-10 19:30:21 -0400

    Day 1 progress

--- Engine/Player.cs
+++ Engine/Player.cs
@@ -8,8 +8,6 @@ namespace Engine
 {
     public class Player
     {
-        public int CurrentHitPoints { get; set; }
-        public int MaximumHitPoints { get; set; }
         public int Gold { get; set; }
         public int ExperiencePoints { get; set; }
         public int Level { get; set; }
